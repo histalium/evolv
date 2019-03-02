35483a44231aa370227286918b7cfcc35fe51a15
histalium <histalium@outlook.com>
2019-03-02 17:53:11 +0100

    draw a creature

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-var c = document.getElementById('c');
-var ctx = c.getContext('2d');
+const c = document.getElementById('c');
+const ctx = c.getContext('2d');
 
 c.width = 1024;
 c.height = 768;
 
-ctx.beginPath();
-ctx.arc(50, 50, 20, 0, Math.PI * 2);
-ctx.stroke();
+const fullCircle = Math.PI * 2;
+
+var creature = {
+  location: {
+    x: 50,
+    y: 50
+  }
+};
+
+const drawCreature = creature => {
+  ctx.beginPath();
+  ctx.arc(creature.location.x, creature.location.y, 20, 0, Math.PI * 2);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(creature.location.x + 20, creature.location.y, 5, 0, Math.PI * 2);
+  ctx.fill();
+};
+
+drawCreature(creature);
